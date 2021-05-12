e692896a7aac9e2fc40bdfb0e509b06e6b7cfad7
Joshua A. Anderson <joaander@umich.edu>
2021-05-12 11:02:19 -0400

    Try explicit locations for packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,6 @@ echo "CFLAGS=${CFLAGS}"
 echo "CXXFLAGS=${CXXFLAGS}"
 
 export CMAKE_PREFIX_PATH=${PREFIX}
-${PYTHON} -m pip install . --ignore-installed --no-cache-dir -vv --install-option="-- ${CMAKE_ARGS}"
+${PYTHON} -m pip install . --ignore-installed --no-cache-dir -vv --install-option="-- ${CMAKE_ARGS} -DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include -DCYTHON_EXECUTABLE=$BUILD_PREFIX/bin/cython -DPYTHON_EXECUTABLE=$BUILD_PREFIX/bin/python3"
 
-ls /Users/runner/miniforge3/conda-bld/freud_* -R
+ls -R /Users/runner/miniforge3/conda-bld/freud_*
