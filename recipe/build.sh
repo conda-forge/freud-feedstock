5252d60ab78490839362400a4fcde20a4033efa3
Joshua A. Anderson <joaander@umich.edu>
2021-05-12 13:28:32 -0400

    Try alternate filtering method

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,10 +13,7 @@ echo "CXXFLAGS=${CXXFLAGS}"
 
 export CMAKE_PREFIX_PATH=${PREFIX}
 
-ls -lR ${SP_DIR}
-
 # Filter CMAKE_PREFIX_PATH out of CMAKE_ARGS because scikit-build needs to set it
-CMAKE_ARGS_FILTERED=$(echo $CMAKE_ARGS | sed -e 's/\-DCMAKE_INSTALL_PREFIX\=\$PREFIX//g')
+CMAKE_ARGS_FILTERED=$(echo $CMAKE_ARGS | sed -e 's/\-DCMAKE_INSTALL_PREFIX\=[^ ]* //g')
 
 ${PYTHON} setup.py install -- ${CMAKE_ARGS_FILTERED}
-#-DNumPy_INCLUDE_DIR=${SP_DIR}/numpy/core/include
