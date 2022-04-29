db629e8649a60d63d1188bb562abcc7c2b26cfde
Joshua A. Anderson <joaander@umich.edu>
2022-04-29 09:14:51 -0400

    Remove changes that don't have any effect.

--- recipe/build_osx_arm64.sh
+++ recipe/build_osx_arm64.sh
@@ -15,13 +15,6 @@ export CMAKE_PREFIX_PATH=${PREFIX}
 
 # Filter CMAKE_PREFIX_PATH out of CMAKE_ARGS because scikit-build needs to set it
 export SKBUILD_CONFIGURE_OPTIONS=$(echo $CMAKE_ARGS | sed -e 's/\-DCMAKE_INSTALL_PREFIX\=[^ ]* //g')
-# Explicitly set the numpy include directory.
-# export SKBUILD_CONFIGURE_OPTIONS="${SKBUILD_CONFIGURE_OPTIONS} -DNumPy_INCLUDE_DIR=$SP_DIR/numpy/core/include"
-
-# echo "SKBUILD_CONFIGURE_OPTIONS=${SKBUILD_CONFIGURE_OPTIONS}"
-# echo "numpy include=${SP_DIR/numpy/core/include}"
-
-# ls -lR $PREFIX/lib
 
 # work around a bug in conda-forge where the installed cython gets an invalid #! line: "#!$BUILD_PREFIX/bin/python"
 # BUILD_PREFIX appears to be first on the path, so use /usr/bin/env python
@@ -32,4 +25,4 @@ mv cython.tmp $BUILD_PREFIX/bin/cython
 chmod ugo+rwx $BUILD_PREFIX/bin/cython
 
 echo "### Performing installation"
-$PYTHON -m pip install . --ignore-installed --no-cache-dir -vv --no-use-pep517 --no-build-isolation
+$PYTHON -m pip install . --ignore-installed --no-cache-dir -vv
